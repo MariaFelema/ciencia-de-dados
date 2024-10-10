27de9a3cbd85d2d22d20b81cf16ff56d3896cb9a
MariaFelema <105368426+MariaFelema@users.noreply.github.com>
2024-10-10 10:24:17 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,6 +7,7 @@ async function visualizarInformacoesGlobais() {
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente 
     ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em media ${dados.tempo_medio} horas conectadas.`
-    console.log(paragrafo)
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 visualizarInformacoesGlobais()
